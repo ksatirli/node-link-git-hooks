f95f314b933828aadea4571b527240ae39625f50
Yury Liavitski <yury.liavitski@il111.ice.local>
2016-03-01 17:01:47 +0100

    Changing code to sync flow, cleaning up

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 'use strict';
 
-var async = require('async');
 var chalk = require('chalk');
 var hooks = require('./lib/hooks');
 
-// handle config
-async.waterfall(
-[
-  function(callback) {
-    // add goodness to payload
-    var payload = {
-      argv: process.argv.slice(2),
+var payload = {
+  argv: process.argv.slice(2),
 
-      directories: {
-        gitHooks: '.git/hooks',
-        userGitHooks: './hooks'
-      }
-    };
-
-    callback(null, payload);
-  },
-
-  hooks.checkGitRepositoryStatus,
-
-  hooks.checkUserHookAvailability,
-
-  hooks.listAvailableHooks,
-
-  hooks.linkAvailableHooks
-
-], function(err) {
-  if (err) {
-    console.error();
-    console.error(chalk.red('Linking could not be completed successfully.'));
-  } else {
-    console.log();
-    console.log(chalk.green('All done.'));
+  directories: {
+    gitHooks: '.git/hooks',
+    userGitHooks: './hooks'
   }
-});
+};
+
+try {
+  hooks.checkGitRepositoryStatus(payload);
+  hooks.checkUserHookAvailability(payload);
+  hooks.listAvailableHooks(payload);
+  hooks.linkAvailableHooks(payload);
+} catch (err) {
+  console.error();
+  console.error(chalk.red('Linking could not be completed successfully.'));
+}
+
+
+console.log();
+console.log(chalk.green('All done.'));
